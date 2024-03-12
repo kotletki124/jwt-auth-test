5219f902f622954ea4caa26cd9e3a02fdcb0584a
kotletki124 <kotletki124@gmail.com>
2024-03-12 20:27:47 +0300

    1

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as path from 'path';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,7 +13,7 @@ async function bootstrap() {
   app.useStaticAssets(path.join(__dirname, '..', 'public'), { index: false });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
